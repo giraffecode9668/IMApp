4189f0d18a1254d4044a27795c6b371eeacdbaa2
giraffecode9668 <ziminlin@126.com>
2019-03-15 12:38:01 +0800

    make 4 fragment of using viewpaper

--- app/src/main/java/com/giraffe/imapp/activity/MainActivity.java
+++ app/src/main/java/com/giraffe/imapp/activity/MainActivity.java
@@ -1,21 +1,97 @@
 package com.giraffe.imapp.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Adapter;
 
 import com.giraffe.imapp.R;
+import com.giraffe.imapp.fragment.ChatsFragment;
+import com.giraffe.imapp.fragment.CommunityFragment;
+import com.giraffe.imapp.fragment.ContactsFragment;
+import com.giraffe.imapp.fragment.SettingFragment;
+import com.giraffe.imapp.url.ViewPagerAdapter;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private MenuItem menuItem;
+    private BottomNavigationViewEx bnve;
+    private List<Fragment> list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationViewEx bnve = findViewById(R.id.bnve);
+        bnve = findViewById(R.id.AM_bnve);
+        viewPager = findViewById(R.id.AM_viewpager);
         bnve.enableAnimation(true);
         bnve.enableShiftingMode(false);
+        bnve.setOnNavigationItemSelectedListener(
+                new BottomNavigationViewEx.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_chats:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.action_contacts:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.action_community:
+                                viewPager.setCurrentItem(2);
+                                break;
+                            case R.id.action_setting:
+                                viewPager.setCurrentItem(3);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    bnve.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = bnve.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        list = new ArrayList<>();
+        list.add(new ChatsFragment());
+        list.add(new ContactsFragment());
+        list.add(new CommunityFragment());
+        list.add(new SettingFragment());
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(),list);
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/giraffe/imapp/url/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.giraffe.imapp.url;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.giraffe.imapp.fragment.ChatsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private FragmentManager mfragmentManager;
+    private final List<Fragment> mFragmentList;
+
+    public ViewPagerAdapter(FragmentManager fm,List<Fragment> list) {
+        super(fm);
+        this.mFragmentList = list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+}
