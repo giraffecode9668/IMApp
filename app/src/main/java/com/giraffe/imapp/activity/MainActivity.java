4c00c19a9dd8baf63d3e2d7ee4a109da19ba092f
giraffecode9668 <ziminlin@126.com>
2019-03-15 08:24:44 +0800

    make bottomnavigation by use of BottomNavigationViewEx

--- app/src/main/java/com/giraffe/imapp/activity/MainActivity.java
+++ app/src/main/java/com/giraffe/imapp/activity/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.giraffe.imapp.R;
-
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationViewEx bnve = findViewById(R.id.bnve);
+        bnve.enableAnimation(true);
+        bnve.enableShiftingMode(false);
     }
 }
